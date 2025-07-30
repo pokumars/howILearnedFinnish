e5bfaf8460dfb2ed208039cbdd480070c2f92b67
Oheneba Poku-Marboah <ohenebakob@hotmail.com>
2025-07-30 16:43:23 +0300

    Refactor filter tags into an enum for better type safety and maintainability, and update episode tags to use the new enum values.

--- src/constants/episodes.ts
+++ src/constants/episodes.ts
@@ -1,13 +1,14 @@
-export const filterTags = [
-  "All Episodes",
-  "Parent",
-  "Student",
-  "Finnish-speaking partner",
-  "Non-Finnish-speaking partner",
-  "A1 to C1",
-  "With Kids",
-  "Immersion method",
-];
+export enum FilterTags {
+  AllEpisodes = "All Episodes",
+  Parent = "Parent",
+  Student = "Student",
+  FinnishSpeakingPartner = "Finnish-speaking partner",
+  NonFinnishSpeakingPartner = "Non-Finnish-speaking partner",
+  A1toC1 = "A1 to C1",
+  WithKids = "With Kids",
+  Immersion = "Immersion method",
+}
+export const filterTags = Object.values(FilterTags);
 
 export const episodes = [
   {
@@ -15,7 +16,7 @@ export const episodes = [
     title: "006: Stop Saying It's Too Hard, That's Why You're Stuck - Emily",
     description:
       "Meet Emily, an entrepreneur and hairdresser who shares her unique strategies, challenges, and experiences that shaped her language-learning process. In this episode, we dive deep into Emily's journey of learning Finnish. From studying abroad to integrating into Finnish society, Emily covers essential topics like vocabulary building, speaking, listening, grammar, reading, and writing. She also discusses the importance of mindset, staying motivated, and leveraging resources like podcasts, books, and online platforms to master Finnish. Learn how Emily overcame the hurdles of learning a new language and how you can apply her insights to your language-learning journey.",
-    tags: ["Student", "A1 to C1", "Immersion method"],
+    tags: [FilterTags.Student, FilterTags.A1toC1, FilterTags.Immersion],
     videoUrl: "https://www.youtube.com/watch?v=t3QWADJcyJI",
     thumbnail: "/thumbnails/6_emily.jpg",
     platforms: [
@@ -40,11 +41,11 @@ export const episodes = [
     description:
       "3 years to go from zero Finnish to law school admission. Check out this interview of how Deborah was able to learn such high level Finnish. In this episode, we delve into the inspiring journey of Deborah Laajanen, a lawyer from the Philippines who managed to get accepted into a Finnish law school despite being in Finland for just a few years. Deborah shares her struggles and strategies for learning Finnish to an advanced level, including the use of resources like Suomen mestari, podcasts, and integrating herself into Finnish society. She also discusses the motivation behind her efforts, the role of discipline, and the importance of setting concrete goals. This conversation is a must-listen for anyone facing the daunting task of learning a new language, especially in a new country.",
     tags: [
-      "Parent",
-      "With Kids",
-      "Immersion method",
-      "Finnish-speaking partner",
-      "Non-Finnish-speaking partner",
+      FilterTags.Parent,
+      FilterTags.WithKids,
+      FilterTags.Immersion,
+      FilterTags.FinnishSpeakingPartner,
+      FilterTags.NonFinnishSpeakingPartner,
     ],
     videoUrl: "https://youtu.be/V0jzUitkKL4?si=AEY553jX9wk0qV_W",
     thumbnail: "/thumbnails/5_Deborah.jpg",
@@ -68,7 +69,7 @@ export const episodes = [
     title: "004: Why Duolingo Failed, Paying for 100 Lunches, and More - Erik",
     description:
       "In this episode, we explore Erik's unique approach to learning Finnish through real-world interactions and practical experiences. Erik shares why traditional language learning apps like Duolingo weren't effective for him and how he discovered more effective methods. He discusses his strategy of paying for lunches to practice Finnish with native speakers, the importance of immersion, and how he overcame common challenges faced by adult language learners. This episode is packed with actionable insights for anyone looking to learn Finnish through authentic experiences.",
-    tags: ["Student", "A1 to C1", "Immersion method"],
+    tags: [FilterTags.Student, FilterTags.A1toC1, FilterTags.Immersion],
     videoUrl: "https://youtu.be/qD6RTlqPmRA?si=fte-t1TsGpENFrCT",
     thumbnail: "/thumbnails/4_Erik.jpg",
     platforms: [
@@ -92,7 +93,7 @@ export const episodes = [
       "003: I Lost Custody of My Kids Because I Couldn't Speak Finnish - Anita Anttila",
     description:
       "Anita shares her heartbreaking yet inspiring story of how language barriers affected her family life and custody situation. This powerful episode explores the real consequences of not being able to communicate in Finnish, especially when it comes to family matters. Anita discusses her journey to learn Finnish, the challenges she faced in the legal system, and how she eventually regained custody of her children. This episode highlights the importance of language learning beyond just personal development - it can literally change lives and family dynamics.",
-    tags: ["Parent", "With Kids", "A1 to C1"],
+    tags: [FilterTags.Parent, FilterTags.WithKids, FilterTags.A1toC1],
     videoUrl: "https://www.youtube.com/watch?v=zcW4ibdkGc4",
     thumbnail: "/thumbnails/3_Anita.jpg",
     platforms: [
@@ -112,7 +113,7 @@ export const episodes = [
     title: "002: How I Achieved Fluency in Finnish in Just One Year",
     description:
       "In 2022, I embarked on a radical experiment — I pretended not to speak English for an entire year in order to immerse myself in Finnish and reach fluency as an adult. My  journey of mastering the Finnish language in just one year starting from a weak level! In this episode of 'How I Learned,' I reveal the key elements that helped me achieve high-level fluency by the end of 2022. From setting specific targets and creating a language immersion environment to utilizing podcasts, books, and articles, I provide actionable tips that can help you learn any language. Learn about the six essential components of language learning—vocabulary, speaking, listening, grammar, reading, and writing—and discover how I overcame challenges, kept myself motivated, and adapted my strategies along the way. Whether you're starting from scratch or looking to refine your skills, this video is packed with valuable advice for achieving your language learning goals. Don’t miss out—watch now and get inspired to take your linguistic abilities to the next level!",
-    tags: ["Student", "A1 to C1", "Immersion method"],
+    tags: [FilterTags.Student, FilterTags.A1toC1, FilterTags.Immersion],
     videoUrl: "https://www.youtube.com/watch?v=mEQKGH0IN_s",
     thumbnail: "/thumbnails/2_Oheneba.jpg",
     platforms: [
